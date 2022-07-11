d76bc03eb1016d96012ecf69953483808dc2f6bc
Yevhenii <freshjs.fte.@gmail.com>
2022-07-11 15:58:16 +0300

    Refactor login and register request

--- client/src/sagas/authSagas.js
+++ client/src/sagas/authSagas.js
@@ -1,25 +1,28 @@
 import { put } from 'redux-saga/effects';
 import ACTION from '../actions/actionTypes';
-import history from '../browserHistory';
 import * as restController from '../api/rest/restController';
 
 export function* loginSaga(action) {
-  yield put({ type: ACTION.AUTH_ACTION_REQUEST });
   try {
-    yield restController.loginRequest(action.data);
+    const response = yield restController.loginRequest(action.data);
     action.history.replace('/');
+
+    const userData = response.data.user;
     yield put({ type: ACTION.AUTH_ACTION_SUCCESS });
+    yield put({ type: ACTION.GET_USER_SUCCESS, data: userData });
   } catch (err) {
     yield put({ type: ACTION.AUTH_ACTION_ERROR, error: err.response });
   }
 }
 
 export function* registerSaga(action) {
-  yield put({ type: ACTION.AUTH_ACTION_REQUEST });
   try {
-    yield restController.registerRequest(action.data);
+    const response = yield restController.registerRequest(action.data);
     action.history.replace('/');
+
+    const userData = response.data.user;
     yield put({ type: ACTION.AUTH_ACTION_SUCCESS });
+    yield put({ type: ACTION.GET_USER_SUCCESS, data: userData });
   } catch (e) {
     yield put({ type: ACTION.AUTH_ACTION_ERROR, error: e.response });
   }

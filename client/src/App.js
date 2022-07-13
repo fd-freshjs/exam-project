03a16eebfc6895d34451d1d8fc3d9853f943aa5e
Yevhenii <freshjs.fte.@gmail.com>
2022-07-13 16:14:06 +0300

    Refactor refresh

--- client/src/App.js
+++ client/src/App.js
@@ -30,10 +30,9 @@ function App () {
   
   useLayoutEffect(() => {
     const refresh = localStorage.getItem(CONSTANTS.REFRESH_TOKEN);
-    
-    const body = { refreshToken: refresh };
-
-    dispatch(authActionRefresh(body));
+    if (refresh) {
+      dispatch(authActionRefresh({ refreshToken: refresh }));
+    }
   }, [dispatch]);
 
   return (
